b071ccdd0c719545a58525831aced679302db89c
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2020-08-29 21:01:53 +0200

    json to object extension

---
+++ Hsf.ApplicationProcess.August2020.Web/Extensions/JsonElementExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace Hsf.ApplicationProcess.August2020.Web.Extensions
+{
+    public static class JsonElementExtensions
+    {
+        public static Tobject Deserialize<Tobject>(this JsonElement element) where Tobject : class
+        {
+            var rawData = element.GetRawText();
+            return JsonSerializer.Deserialize<Tobject>(rawData);
+        }
+    }
+}
