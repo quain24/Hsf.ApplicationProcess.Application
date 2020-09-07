778454a2bd5e486c097f9f580b9fdd489e24ad69
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2020-09-07 03:27:14 +0200

    Translation update,

--- Hsf.ApplicationProcess.August2020.Blazor/i18ntext/@types/Hsf.ApplicationProcess.August2020.Blazor.I18nText.local.cs
+++ Hsf.ApplicationProcess.August2020.Blazor/i18ntext/@types/Hsf.ApplicationProcess.August2020.Blazor.I18nText.local.cs
@@ -67,19 +67,31 @@ namespace Hsf.ApplicationProcess.August2020.Blazor.I18nText
         /// <summary>"Success!"</summary>
         public string toast_h_applicant_added;
 
+        /// <summary>"Success!"</summary>
+        public string toast_h_applicant_updated;
+
         /// <summary>"API rejected sent data!"</summary>
         public string toast_h_bad_param_sent;
 
         /// <summary>"Connection error!"</summary>
         public string toast_h_connection_err;
 
+        /// <summary>"ID not found."</summary>
+        public string toast_h_not_found;
+
         /// <summary>"Applicant has been added: "</summary>
         public string toast_m_applicant_added;
+
+        /// <summary>"Applicant has been updated: "</summary>
+        public string toast_m_applicant_updated;
 
         /// <summary>"Incorrect parameters were sent: "</summary>
         public string toast_m_bad_param_sent;
 
         /// <summary>"Could not add applicant to database - API is not reachable. Click to try again."</summary>
         public string toast_m_connection_err;
+
+        /// <summary>"This id does not exist in database - ID: "</summary>
+        public string toast_m_not_found;
     }
 }
